8fbb968df7552054b69ac7c32715934e79917ce7
Lyly <lylyle@pdx.edu>
2026-06-10 16:00:56 -0700

    update

--- script.js
+++ script.js
@@ -30,3 +30,20 @@ $('h1').click(function(){
         }, Math.round(Math.random() * i * 600));
       });
     }, false);
+
+gsap.registerPlugin(MotionPathPlugin);
+
+gsap.to("#rect", {
+  duration: 5, 
+  repeat: 12,
+  repeatDelay: 3,
+  yoyo: true,
+  ease: "power1.inOut",
+  motionPath:{
+    path: "#path",
+    align: "#path",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5]
+  }
+});
+
