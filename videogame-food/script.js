10e40deadcc8e7781231fbafe439dd0d7ac42810
alannarisse <alannarisse@users.noreply.github.com>
2026-06-10 17:09:51 -0700

    layout fixes

--- videogame-food/script.js
+++ videogame-food/script.js
@@ -1,64 +1,3 @@
-$(Tab-1).ready(function(){
-	
-	// Lift card and show stats on Mouseover
-	$('#product-card').hover(function(){
-			$(this).addClass('animate');
-			$('div.carouselNext, div.carouselPrev').addClass('visible');			
-		 }, function(){
-			$(this).removeClass('animate');			
-			$('div.carouselNext, div.carouselPrev').removeClass('visible');
-	});	
-	
-	// Flip card to the back side
-	$('#view_details').click(function(){		
-		$('div.carouselNext, div.carouselPrev').removeClass('visible');
-		$('#product-card').addClass('flip-10');
-		setTimeout(function(){
-			$('#product-card').removeClass('flip-10').addClass('flip90').find('div.shadow').show().fadeTo( 80 , 1, function(){
-				$('#product-front, #product-front div.shadow').hide();			
-			});
-		}, 50);
-		
-		setTimeout(function(){
-			$('#product-card').removeClass('flip90').addClass('flip190');
-			$('#product-back').show().find('div.shadow').show().fadeTo( 90 , 0);
-			setTimeout(function(){				
-				$('#product-card').removeClass('flip190').addClass('flip180').find('div.shadow').hide();						
-				setTimeout(function(){
-					$('#product-card').css('transition', '100ms ease-out');			
-					$('#cx, #cy').addClass('s1');
-					setTimeout(function(){$('#cx, #cy').addClass('s2');}, 100);
-					setTimeout(function(){$('#cx, #cy').addClass('s3');}, 200);				
-					$('div.carouselNext, div.carouselPrev').addClass('visible');				
-				}, 100);
-			}, 100);			
-		}, 150);			
-	});			
-	
-	// Flip card back to the front side
-	$('#flip-back').click(function(){		
-		
-		$('#product-card').removeClass('flip180').addClass('flip190');
-		setTimeout(function(){
-			$('#product-card').removeClass('flip190').addClass('flip90');
-	
-			$('#product-back div.shadow').css('opacity', 0).fadeTo( 100 , 1, function(){
-				$('#product-back, #product-back div.shadow').hide();
-				$('#product-front, #product-front div.shadow').show();
-			});
-		}, 50);
-		
-		setTimeout(function(){
-			$('#product-card').removeClass('flip90').addClass('flip-10');
-			$('#product-front div.shadow').show().fadeTo( 100 , 0);
-			setTimeout(function(){						
-				$('#product-front div.shadow').hide();
-				$('#product-card').removeClass('flip-10').css('transition', '100ms ease-out');		
-				$('#cx, #cy').removeClass('s1 s2 s3');			
-			}, 100);			
-		}, 150);			
-		
-	});	
 
 	
 	/* ----  Image Gallery Carousel   ---- */
@@ -97,7 +36,6 @@ $(Tab-1).ready(function(){
 	    isAnimating = true;
 		setTimeout(function(){isAnimating = false;}, 300);			
 	});
-});
 
 function toggleDrawer() {
 	const drawer = document.getElementById("nav-drawer");
